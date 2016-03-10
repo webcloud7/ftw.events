ad5b20629eab27182d95bd3763173bd47449e394
Jonas Baumann <jone@jone.ch>
2016-03-10 09:53:32 +0100

    add generic setup profile.

--- ftw/events/testing.py
+++ ftw/events/testing.py
@@ -1,6 +1,7 @@
 from ftw.builder.testing import BUILDER_LAYER
 from ftw.builder.testing import functional_session_factory
 from ftw.builder.testing import set_builder_session_factory
+from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
@@ -19,6 +20,9 @@ class EventsLayer(PloneSandboxLayer):
             '  <includePluginsOverrides package="plone" />'
             '</configure>',
             context=configurationContext)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'ftw.events:default')
 
 
 EVENTS_FIXTURE = EventsLayer()
--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
 
     install_requires=[
         'Plone',
+        'ftw.upgrade',
         'setuptools',
     ],
 
