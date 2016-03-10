4821d8a6208f8b096577391e64158983b61d46da
Jonas Baumann <jone@jone.ch>
2016-03-10 09:48:16 +0100

    create package.

---
+++ ftw/events/testing.py
@@ -0,0 +1,28 @@
+from ftw.builder.testing import BUILDER_LAYER
+from ftw.builder.testing import functional_session_factory
+from ftw.builder.testing import set_builder_session_factory
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from zope.configuration import xmlconfig
+
+
+class EventsLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, BUILDER_LAYER)
+
+    def setUpZope(self, app, configurationContext):
+        xmlconfig.string(
+            '<configure xmlns="http://namespaces.zope.org/zope">'
+            '  <include package="z3c.autoinclude" file="meta.zcml" />'
+            '  <includePlugins package="plone" />'
+            '  <includePluginsOverrides package="plone" />'
+            '</configure>',
+            context=configurationContext)
+
+
+EVENTS_FIXTURE = EventsLayer()
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(EVENTS_FIXTURE,
+           set_builder_session_factory(functional_session_factory)),
+    name="ftw.events:functional")
---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0.dev0'
+
+tests_require = [
+    'ftw.builder',
+    'ftw.testbrowser',
+    'ftw.testing',
+    'plone.app.testing',
+    'plone.testing',
+]
+
+setup(
+    name='ftw.events',
+    version=version,
+    description='Events with simplelayout.',
+    long_description=open('README.rst').read() + '\n' + open(
+        os.path.join('docs', 'HISTORY.txt')).read(),
+
+    classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    keywords='ftw events',
+    author='4teamwork AG',
+    author_email='mailto:info@4teamwork.ch',
+    url='https://github.com/4teamwork/ftw.events',
+    license='GPL2',
+
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['ftw', ],
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+        'Plone',
+        'setuptools',
+    ],
+
+    tests_require=tests_require,
+    extras_require={'tests': tests_require},
+
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """)
