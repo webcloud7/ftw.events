179d9ba9937fe7412a4ce3cd1d785d94ada1c585
Jonas Baumann <jone@jone.ch>
2016-03-13 09:50:56 +0100

    move event's "timezone" field to the default fieldset.

--- ftw/events/contents/eventpage.py
+++ ftw/events/contents/eventpage.py
@@ -1,9 +1,19 @@
 from ftw.events.interfaces import IEventPage
+from plone.app.event.dx.behaviors import IEventBasic
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.content import Container
 from plone.directives import form
+from plone.supermodel.interfaces import FIELDSETS_KEY
 from zope.interface import alsoProvides
 from zope.interface import implements
+
+
+# Move plone.app.event's default field "timezone" to the default schema,
+# since all other fields are in the default schema and we do not want
+# an extra schema only for the "timezone" field.
+for fieldset in IEventBasic.queryTaggedValue(FIELDSETS_KEY, []):
+    if 'timezone' in fieldset.fields:
+        fieldset.fields.remove('timezone')
 
 
 class IEventPageSchema(form.Schema):
