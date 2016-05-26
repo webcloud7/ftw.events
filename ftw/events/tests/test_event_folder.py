e5454d1ea2dd335358103efce1efd9711eb825a0
Martin Bächtold <martin@baechtold.me>
2016-05-26 08:04:49 +0200

    Add subscription handler for the creation of event folders.
    The handler will automatically add an event listing block once the event folder has been created.

--- ftw/events/tests/test_event_folder.py
+++ ftw/events/tests/test_event_folder.py
@@ -1,3 +1,5 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.events.tests import FunctionalTestCase
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
@@ -15,3 +17,13 @@ class TestEventFolder(FunctionalTestCase):
         browser.fill({'Title': 'Activities'}).save()
         statusmessages.assert_no_error_messages()
         self.assertEquals('http://nohost/plone/activities/view', browser.url)
+
+    @browsing
+    def test_event_folder_has_empty_listing_block(self, browser):
+        self.grant('Manager')
+        folder = create(Builder('event folder'))
+        browser.login().visit(folder, view='@@simplelayout-view')
+        self.assertEqual(
+            ['No content available'],
+            browser.css('.sl-layout .ftw-events-eventlistingblock').text
+        )
